790f6c0df91c5af6798c826b1cc1b85305d9667e
Diana Del Gaudio <ddelgaudio347@gmail.com>
2026-05-03 19:28:40 -0400

    began optimizing TwoSum

--- studying_2026/src/afterOptimizing/TwoSum.java
+++ studying_2026/src/afterOptimizing/TwoSum.java
@@ -1,5 +1,20 @@
 package afterOptimizing;
 
+import java.util.Arrays;
+
 public class TwoSum {
+	//optimized solution -- Hashmaps!
+	
+	public static int[] twoSum(int[] nums, int target) {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {3, 4, 6, 1, 0};
+        int target = 6;
+
+        int[] result = twoSum(nums, target);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+	}
 
 }
