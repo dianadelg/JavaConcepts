0e4e3fb2fb0b5f8658c1daab48f354279c6a3c14
Diana Del Gaudio <ddelgaudio347@gmail.com>
2026-05-04 18:15:29 -0400

    still needs notes on runtime

--- studying_2026/src/afterOptimizing/TwoSum.java
+++ studying_2026/src/afterOptimizing/TwoSum.java
@@ -1,16 +1,32 @@
 package afterOptimizing;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class TwoSum {
 	//optimized solution -- Hashmaps!
 	
 	public static int[] twoSum(int[] nums, int target) {
-		return null;
+		//convert array to Hashmap
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int[] result = new int[2];
+		for(int i=0; i< nums.length; i++) {
+			//check for complement (or target - current ind) existing in array
+			//store number and index as you go through array
+			int complement = target - nums[i];
+			if(map.containsKey(complement)) {
+				result[0] = map.get(complement); //smaller index first
+				result[1] = i;
+				return result;
+			}else {
+				map.put(nums[i],i);
+			}
+		}
+		
+		throw new IllegalArgumentException("No solution found"); //should not get here based on prompt
 	}
 	
 	public static void main(String[] args) {
-		int[] nums = {3, 4, 6, 1, 0};
+		int[] nums = {7, 0, 2, 4, 5};
         int target = 6;
 
         int[] result = twoSum(nums, target);
