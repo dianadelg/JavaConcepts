f5f5699f24f35ccf91fdc514e51b8f9459eaa82a
Diana Del Gaudio <ddelgaudio347@gmail.com>
2026-05-09 20:12:16 -0400

    added notes

--- studying_2026/src/beforeOptimizing/LongestSubstring.java
+++ studying_2026/src/beforeOptimizing/LongestSubstring.java
@@ -10,9 +10,9 @@ public class LongestSubstring {
     	//check if n+1....n letters form a string without any duplicate letters
     	//need to keep track of current string being built and lookup letters -- hashset -- does it exist or not
     	//pwwkew
-    	String currentLongest = "";
-    	String currentStringBuilder = "";
-    	HashSet<Character> chars = new HashSet<>();
+    	String currentLongest = ""; //TIP: do not store string, just store lengths (it only asks for lengths)
+    	String currentStringBuilder = ""; 
+    	HashSet<Character> chars = new HashSet<>(); //O(n) at worst for space
     	for(int j=0; j< s.length(); j++) { //O(n)
 	    	for(int i=j; i< s.length(); i++) {//O(n)
 	    		char curr = s.charAt(i);
@@ -42,7 +42,7 @@ public class LongestSubstring {
 		System.out.println(lengthOfLongestSubstring("abcabc"));
 		System.out.println(lengthOfLongestSubstring("bbbbb"));
 		System.out.println(lengthOfLongestSubstring("pwwkew"));
-		System.out.println(lengthOfLongestSubstring("abcde"));
+		System.out.println(lengthOfLongestSubstring("abcde")); //this only works as a weird side effect, because we should reset every j
 	}
 
 }
