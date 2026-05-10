d22498e5215f70d2df1581ac218e3e63681f7143
Diana Del Gaudio <ddelgaudio347@gmail.com>
2026-05-09 20:05:46 -0400

    BRUTE FORCE BRUTE FORCE BRUTE

--- studying_2026/src/beforeOptimizing/LongestSubstring.java
+++ studying_2026/src/beforeOptimizing/LongestSubstring.java
@@ -9,35 +9,40 @@ public class LongestSubstring {
     	//algo. For each letter n in string
     	//check if n+1....n letters form a string without any duplicate letters
     	//need to keep track of current string being built and lookup letters -- hashset -- does it exist or not
-    	
+    	//pwwkew
     	String currentLongest = "";
     	String currentStringBuilder = "";
     	HashSet<Character> chars = new HashSet<>();
-    	for(int i=0; i< s.length(); i++) {
-    		char curr = s.charAt(i);
-    		if(chars.contains(curr)||i==s.length()-1) { //means on last letter
-    			//means it exists in the substring, therefore, reset
-    			if(currentLongest.length() < currentStringBuilder.length()) {
-    				//set current String Builder to current Longest
-    				currentLongest=currentStringBuilder;
-    				currentStringBuilder="";
-    				chars.clear();
-    			}
-    		}else {
-    			//means does not contain yet
-    			chars.add(curr);
-    			currentStringBuilder+=curr;
-    		}
+    	for(int j=0; j< s.length(); j++) { //O(n)
+	    	for(int i=j; i< s.length(); i++) {//O(n)
+	    		char curr = s.charAt(i);
+	    		if(chars.contains(curr)) {
+	    			//means it exists in the substring, therefore, reset
+	    			if(currentLongest.length() < currentStringBuilder.length()) {
+	    				//set current String Builder to current Longest
+	    				currentLongest=currentStringBuilder;
+	    				currentStringBuilder="";
+	    				chars.clear();
+	    				break;
+	    			}
+	    		}else {
+	    			//means does not contain yet
+	    			chars.add(curr);
+	    			currentStringBuilder+=curr;
+	    		}
+	    	}
     	}
     	System.out.println(currentLongest);
         return currentLongest.length();
     }
+    //Time O(n). Space -- longest substring could be a string of O(n^2) at most (entire string)
 	
 	public static void main (String[] args) {
 		//Given a string s, find the length of the longest substring without duplicate characters.
 		System.out.println(lengthOfLongestSubstring("abcabc"));
 		System.out.println(lengthOfLongestSubstring("bbbbb"));
 		System.out.println(lengthOfLongestSubstring("pwwkew"));
+		System.out.println(lengthOfLongestSubstring("abcde"));
 	}
 
 }
