3ee4064dffa6072efc3dc23e4418d8ec6e2b9983
Diana Del Gaudio <ddelgaudio347@gmail.com>
2026-06-02 20:14:46 -0400

    beforeOptimized MergeTwoLists complete :D

--- studying_2026/src/beforeOptimizing/MergeTwoSortedLists.java
+++ studying_2026/src/beforeOptimizing/MergeTwoSortedLists.java
@@ -23,38 +23,67 @@ public class MergeTwoSortedLists {
 		ListNode solution = new ListNode(0,null);
 		ListNode solutionPtr = solution; 
 		
-		if(ptr1 == null || ptr2 == null) {
+		if(ptr1 == null || ptr2 == null) { //preliminary check for empty list
 			if(ptr1 == null) {
 				return ptr2;
 			}else {
 				return ptr1;
 			}
 		}
-		while(ptr1.next!=null && ptr2.next!=null) {
+		while(ptr1!=null || ptr2!=null) {
 			//can we merge in one list?
 			//else means still data
-			if(ptr1.val>ptr2.val) {
-				solutionPtr.next = new ListNode(ptr1.val, null);
-				ptr1=ptr1.next;
-			}else {
-				//includes if values are equal
-				solutionPtr.next = new ListNode(ptr1.val, null);
+			if(ptr1==null) {
+				solutionPtr.next = new ListNode(ptr2.val, null);
+				solutionPtr=solutionPtr.next;
 				ptr2=ptr2.next;
 			}
-		}
-		//if next one is null, then return remaining other list
-		if(ptr1.next==null) {
-			//means still stuff for ptr2
+			else if(ptr2==null) {
+				solutionPtr.next = new ListNode(ptr1.val, null);
+				solutionPtr=solutionPtr.next;
+				ptr1=ptr1.next;
+			}
+			else if(ptr1.val>ptr2.val) {
+				solutionPtr.next = new ListNode(ptr2.val, null);
+				solutionPtr=solutionPtr.next;
+				ptr2=ptr2.next;
+				solutionPtr.next = new ListNode(ptr1.val, null); //THIS IS DANGEROUS ADDING TWO NODES AT ONCE
+				//because what if we add
+				/*
+					list1 = [1, 10]
+					list2 = [2, 3, 4]
+					
+					Code might do -- because 3<10 but we didn't check for 10 against 4
+					
+					1, 2, 3, 10, 4
+					
+					because it adds 10 before checking 4. 
+				 */
+				solutionPtr=solutionPtr.next;
+				ptr1=ptr1.next;
+			}else {
+				solutionPtr.next = new ListNode(ptr1.val, null);
+				solutionPtr=solutionPtr.next;
+				ptr1=ptr1.next;
+				solutionPtr.next = new ListNode(ptr2.val, null);
+				solutionPtr=solutionPtr.next;
+				ptr2=ptr2.next;
+			}
 			
-		}else if(ptr2.next==null) {
-			//means still stuff for ptr1
-			
-		}else {
-			//return merged list
-			
+			//I ALSO DIDN'T READ INSTRUCTIONS AGAIN...need to stop just jumping into code
+			//I think I get excited lol
+			//this is about SPLICING the nodes, not creating new ones
+			//If I did that, would be O(1) space complexity because reusing nodes
+			//and runtime still would've been O(m+n), since we have to visit each node
 		}
 		
-		return null;
+		return solution.next;
+		
+		//runtime -- O(m+n), where m is the length of one list and n is the other, in the worst case, we would traverse all numbers in both lists
+		//in the best case, one is null and we just return the other list
+		//in this algorithm, we check every number until the end of the list
+		
+		//space complexity -- also O(m+n), where the list returned is m+n nodes/numbers
 		
 	}
 	
@@ -74,6 +103,24 @@ public class MergeTwoSortedLists {
 		ListNode list3 = null;
 		System.out.println("-------------Merged List------------");
 		traverseList(mergeTwoLists(list1, list2));
+		System.out.println();
+		
+		
+		//case 2
+		ListNode numbers0 = new ListNode(6, null);
+		ListNode numbers1 = new ListNode(5, numbers0);
+		ListNode numbers2 = new ListNode(4, numbers1);
+		ListNode numbers6 = new ListNode(3, numbers2);
+		ListNode lists1 = new ListNode(3, numbers6);
+		traverseList(lists1);
+		System.out.println();
+		ListNode numbers4 = new ListNode(5, null);
+		ListNode numbers5 = new ListNode(4, numbers4);
+		ListNode lists2 = new ListNode(1, numbers5);
+		traverseList(lists2);
+		System.out.println();
+		System.out.println("-------------Merged List------------");
+		traverseList(mergeTwoLists(lists1, lists2));
 	}
 	
 	public static void traverseList(ListNode head) {
