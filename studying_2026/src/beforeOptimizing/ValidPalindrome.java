66ed58817598ea907b1904247ac8e8ad4534ee4f
Diana Del Gaudio <ddelgaudio347@gmail.com>
2026-09-05 20:19:31 -0400

    runtime/space cleanup
    I made some boo boos

--- studying_2026/src/beforeOptimizing/ValidPalindrome.java
+++ studying_2026/src/beforeOptimizing/ValidPalindrome.java
@@ -11,29 +11,32 @@ public class ValidPalindrome {
 	 * Notes: empty string is a palindrome
 	 * Need to remove spaces, punctuation, capitalization doesn't matter
 	 * 
-	 * consider \n and \t, maybe an edge case?
+	 * consider \n and \t, maybe an edge case? -- CAVEAT: s consists only of printable ASCII characters.
 	 */
 	
     public static boolean isPalindrome(String s) {
         //make to lower case
-    	s = s.toLowerCase(); //O(1) space and complexity
+    	s = s.toLowerCase().replace(" ", ""); //O(n) space -- STRINGS ARE IMMUTABLE -- and O(2n) --> O(n)complexity
     	
-    	//remove spaces
-    	s = s.replace(" ", ""); //O(n) runtime, O(1) space
-    	
-    	if(s.equals("")) {
+    	if(s.equals("")) { //could do s.isEmpty() --> O(1) but this is also O(1)
     		//empty string, return true
     		return true;
     	}
     	
+    	//could I chain all of these replace, lowercase, replaceAll together to clean it up?
+    
+    	
     	//remove punctuation
-    	s = s.replaceAll("\\p{Punct}", ""); //O(n) runtime, O(1) space
+    	s = s.replaceAll("\\p{Punct}", ""); //O(n) runtime, O(n)space -- strings are immutable
     	
     	for(int i=0; i<s.length()/2; i++){ //O(n)/2 -- O(n)
-    		if(s.charAt(i)!=s.charAt(s.length()-i-1)) { //O(1)
+    		if(s.charAt(i)!=s.charAt(s.length()-i-1)) { //O(1) -- string length is O(1) because java stores string length as a field so it's easy to lookup
+    			//charAt is direct array indexing, not a traversal --> O(1)
     			return false;
     		}
     	} //loop is O(n) * O(1) = O(n)
+    	//space complexity of loop -- O(1) 
+    	//O(1) space because the loop only uses a fixed number of primitive variables (i, and the temporary chars from charAt) that don't grow with the input size — it reads from the existing string instead of creating a new one.
     	return true;
     	
     	//then two pointers like palindrome number 
